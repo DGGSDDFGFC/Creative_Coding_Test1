b029962b5ea415a051b3f6849b5e677d290cdf67
DGGSDDFGFC <129002156+DGGSDDFGFC@users.noreply.github.com>
2026-04-13 12:39:02 +0100

    Add files via upload

--- ca-cc-luka-paranyak/rand_faces.gd
+++ ca-cc-luka-paranyak/rand_faces.gd
@@ -3,6 +3,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+
 	pass # Replace with function body.
 
 
@@ -10,6 +11,16 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 	
-func draw() -> void:
+func _draw() -> void:
+	draw_face()
+	pass
 	
+func draw_face():
+	var posx = randf_range(0, 1000)
+	var posy = randf_range(0, 500)
+	var radius = randi_range(10, 75)
+	var eyes = randi_range(2, 6)
+	var color = randf_range(0, 1)
+	var happy = randi_range(1, 2) # 1 is happy 2 is sad
+	draw_circle(Vector2(posx, posy), radius, Color.BLACK, false)
 	pass
